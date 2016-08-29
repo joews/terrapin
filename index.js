ea593048b6a7558d128b2455ab08e1b92b270a5a
Joe Whitfield-Seed <joe.ws@ripjar.com>
2016-08-29 22:44:42 +0100

    Moar biomes!

--- index.js
+++ index.js
@@ -119,22 +119,83 @@ function generate() {
   render3d();
 }
 
-// TODO more biomes, use moisture
+// credit Amit Patel
+// http://www.redblobgames.com/maps/terrain-from-noise/
 function biome(e, m) {
-  if (e < 0.05) return WATER;
-  else return LAND;
+  if (e < 0.1) return OCEAN;
+  if (e < 0.12) return BEACH;
+
+  if (e > 0.8) {
+    if (m < 0.1) return SCORCHED;
+    if (m < 0.2) return BARE;
+    if (m < 0.5) return TUNDRA;
+    return SNOW;
+  }
+
+  if (e > 0.6) {
+    if (m < 0.33) return TEMPERATE_DESERT;
+    if (m < 0.66) return SHRUBLAND;
+    return TAIGA;
+  }
+
+  if (e > 0.3) {
+    if (m < 0.16) return TEMPERATE_DESERT;
+    if (m < 0.50) return GRASSLAND;
+    if (m < 0.83) return TEMPERATE_DECIDUOUS_FOREST;
+    return TEMPERATE_RAIN_FOREST;
+  }
+
+  if (m < 0.16) return SUBTROPICAL_DESERT;
+  if (m < 0.33) return GRASSLAND;
+  if (m < 0.66) return TROPICAL_SEASONAL_FOREST;
+  return TROPICAL_RAIN_FOREST;
 }
 
-function color(r, g, b, a = 0xFF) {
+function rgba(r, g, b, a = 0xFF) {
   return ((a & 0xFF) << 24)
     | ((b & 0xFF) << 16)
     | ((g & 0xFF) << 8)
     | (r & 0xFF);
 }
 
-// Biomes as 32bit rgba integers
-const WATER = color(75, 75, 127);
-const LAND = color(100, 150, 85);
+// hex: 24-bit hex color code
+function hex(hex, alpha = 0xFF) {
+  const r = hex >> 16 & 0xFF;
+  const g = hex >> 8 & 0xFF;
+  const b = hex & 0xFF;
+  return rgba(r, g, b, alpha);
+}
+
+// From https://github.com/amitp/mapgen2/blob/master/mapgen2.as
+// Credit Amit Patel
+const OCEAN = hex(0x44447a);
+const COAST = hex(0x33335a);
+const LAKESHORE = hex(0x225588);
+const LAKE = hex(0x336699);
+const RIVER = hex(0x225588);
+const MARSH = hex(0x2f6666);
+const ICE = hex(0x99ffff);
+const BEACH = hex(0xa09077);
+const ROAD1 = hex(0x442211);
+const ROAD2 = hex(0x553322);
+const ROAD3 = hex(0x664433);
+const BRIDGE = hex(0x686860);
+const LAVA = hex(0xcc3333);
+const SNOW = hex(0xffffff);
+const TUNDRA = hex(0xbbbbaa);
+const BARE = hex(0x888888);
+const SCORCHED = hex(0x555555);
+const TAIGA = hex(0x99aa77);
+const SHRUBLAND = hex(0x889977);
+const TEMPERATE_DESERT = hex(0xc9d29b);
+const TEMPERATE_RAIN_FOREST = hex(0x448855);
+const TEMPERATE_DECIDUOUS_FOREST = hex(0x679459);
+const GRASSLAND = hex(0x88aa55);
+const SUBTROPICAL_DESERT = hex(0xd2b98b);
+const TROPICAL_RAIN_FOREST = hex(0x337755);
+const TROPICAL_SEASONAL_FOREST = hex(0x559945);
+
+
 
 function initThree() {
   scene = new THREE.Scene();
@@ -152,7 +213,7 @@ function initThree() {
   });
 
   renderer.setSize(width, height);
-  renderer.setClearColor( 0xeeeeee );
+  renderer.setClearColor( 0x333333 );
 
   const ambientLight = new THREE.AmbientLight(0xE4D2AF, 0.3);
   scene.add(ambientLight);
